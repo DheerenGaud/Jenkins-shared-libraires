1f27efd4d0c4f75d2ac98a97e911e0bffca5bb89
Dheeren <110242985+DheerenGaud@users.noreply.github.com>
2025-10-02 16:55:05 +0530

    Update dockerPush.groovy

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,13 +1,9 @@
 def call(String imageName, String dockerRepo, String credentialsId) {
-    steps {
-        script {
-            withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
-                sh """
-                    echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
-                    docker image tag ${imageName} ${dockerRepo}:latest
-                    docker push ${dockerRepo}:latest
-                """
-            }
-        }
+    withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+        sh """
+            echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+            docker tag ${imageName}:latest ${dockerRepo}:latest
+            docker push ${dockerRepo}:latest
+        """
     }
 }
