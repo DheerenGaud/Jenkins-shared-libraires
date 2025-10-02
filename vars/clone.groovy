4710e4626330d8739e9800f8a399961d461a1334
Dheeren <110242985+DheerenGaud@users.noreply.github.com>
2025-10-02 16:20:20 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,5 +1,5 @@
 def clone(String url,String branch){
                 echo "clone the project"
-                git url:url,
-                branch : branch
+                git url:"${url}",
+                branch : "${branch}"
 }
